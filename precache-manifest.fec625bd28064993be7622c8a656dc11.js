dd633a23648a5824b67f9e9b1611df3676e80ade
Kavithakarneni <kavitha924646@gmail.com>
2020-05-02 18:41:28 +0530

    Updates

---
+++ precache-manifest.fec625bd28064993be7622c8a656dc11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1988604079ea311b9c8cf6be682cbc12",
+    "url": "/http//Kavithakarneni.github.io/myproject/index.html"
+  },
+  {
+    "revision": "c3e93d0966157ef9bdc9",
+    "url": "/http//Kavithakarneni.github.io/myproject/static/css/main.3fb67485.chunk.css"
+  },
+  {
+    "revision": "2bf4133c9908f104f448",
+    "url": "/http//Kavithakarneni.github.io/myproject/static/js/2.f1cd53a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//Kavithakarneni.github.io/myproject/static/js/2.f1cd53a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e93d0966157ef9bdc9",
+    "url": "/http//Kavithakarneni.github.io/myproject/static/js/main.9dbe0553.chunk.js"
+  },
+  {
+    "revision": "bb87909c15fd3697e3bf",
+    "url": "/http//Kavithakarneni.github.io/myproject/static/js/runtime-main.cd405e15.js"
+  }
+]);
